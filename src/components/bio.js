58b03939549b052ccee018ad09adfb207c05792a
iamvishal345 <vishalagnihotri345@gmail.com>
2020-09-27 21:57:19 +0530

    Added Linkedin badge

--- src/components/bio.js
+++ src/components/bio.js
@@ -62,7 +62,7 @@ const Bio = () => {
             rel="noreferrer"
             href={`https://www.linkedin.com/in/${social?.linkedin || ``}`}
           >
-            Connect with me on LinkedIn
+            <img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" />
           </a>
         </p>
       )}
